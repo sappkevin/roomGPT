4937b68f15807042b3c724f538aecc74c7bec47e
sappkevin <109696421+sappkevin@users.noreply.github.com>
2023-03-15 15:02:38 -0500

    Changed remaining to reflect rate limit of 100
    Made change to remaining rate limit

--- pages/api/remaining.ts
+++ pages/api/remaining.ts
@@ -23,7 +23,7 @@ export default async function handler(
     (await redis?.get(`@upstash/ratelimit:${identifier!}:${bucket}`)) || 0;
 
   const remainingGenerations =
-    Number(usedGenerations) > 3 ? 0 : 3 - Number(usedGenerations);
+    Number(usedGenerations) > 100 ? 0 : 100 - Number(usedGenerations);
 
   return res.status(200).json({ remainingGenerations });
 }
