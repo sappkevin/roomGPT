fc92f25b808cbbb407c5fceab78a83a734858ba1
sappkevin <109696421+sappkevin@users.noreply.github.com>
2023-03-17 23:42:21 -0500

    revert changes

--- pages/api/generate.ts
+++ pages/api/generate.ts
@@ -62,67 +62,8 @@ export default async function handler(
   }
 
   const { imageUrl, theme, room } = req.body;
-  //Decide on which prompt to use based on room type
-  let replicatePrompt = "";
-  let replicateMainPrompt = "";
-  let genericprompt = `a [${theme.toLowerCase()}] [${room.toLowerCase()}]`
-  
-  if (room === "Gaming Room") {
-  //  Gaming Room
-    replicatePrompt =  "a room for gaming with gaming computers, gaming consoles, and gaming chairs"
-    replicateMainPrompt = `Editorial Style [${room.toLowerCase()}] photo, Monochromatic, Crate and Barrel furniture, Cool Neon lighting, home speaker system, Vintage gaming posters on wall, Penthouse, earthy tone painted walls, Evening, Sleek, Contemporary —ar 16:9`
-  } 
-  else if (theme === "Vintage") {
-  //  Vintage Prompt
-    replicateMainPrompt = `Editorial Style [${room.toLowerCase()}] photo, Bird's eye, luxury Mid-century modern, [${room.toLowerCase()}], Bar stools, Leather, wood, Wide shot of dining area, Earth tones, Design Within Reach, Natural light, Mid-century home, Morning, Retro, Mid-century modern —ar 16:9`
-  } 
-  else if (theme === "Minimalist") {
-  //  Minimalist Room
-    replicateMainPrompt = `Editorial Style [${room.toLowerCase()}] photo, High angle, Minimalist, [${room.toLowerCase()}], Marble, wood, Close-up of food, Neutral tones, Williams-Sonoma, Natural light, Home, Morning, Cozy, Modern —ar 16:9`
-  } 
-  else if (theme === "Rustic") {
-  //  Rustic Room
-    replicateMainPrompt = `Editorial Style [${room.toLowerCase()}] photo, Eye level, Rustic, [${room.toLowerCase()}], Farmhouse sink, Stone, wood, Close-up of dish, Earth tones, Le Creuset, Warm light, Country house, Afternoon, Warm, Traditional —ar 16:9`
-  } 
-  else if (theme === "Industrial") {
-  //  Industrial Room
-    replicateMainPrompt = `Editorial Style [${room.toLowerCase()}] photo, Low angle, Industrial, [${room.toLowerCase()}], Stainless steel appliances, Concrete, metal, Close-up of cookware, Cool tones, Artificial light, Loft, Evening, Moody, Contemporary —ar 16:9`
-  } 
-  else if (theme === "Scandinavian") {
-    //  Scandinavian Room
-      replicateMainPrompt = `Editorial Style [${room.toLowerCase()}] photo, Bird's eye, Scandinavian, [${room.toLowerCase()}], Breakfast nook, White, wood, Wide shot of [${room.toLowerCase()}], Pastel colors, IKEA, Natural light, Apartment, Morning, Bright, Modern —ar 16:9`
-    } 
-  else if (theme === "Modern") {
-  //  Modern Room
-    replicateMainPrompt = `Editorial Style [${room.toLowerCase()}] photo, Worm's eye, Modern, [${room.toLowerCase()}], Pendant lights, Glass, metal, Close-up of countertop, Monochromatic, Crate and Barrel, Cool light, Penthouse, Evening, Sleek, Contemporary —ar 16:9`
-  }
-  else if (theme === "Traditional") {
-    //  Professional Room
-      replicateMainPrompt = `Editorial Style [${room.toLowerCase()}] photo, Eye level, Traditional, [${room.toLowerCase()}], Range hood, Tile, wood, Close-up of utensils, Warm tones, Natural light, Victorian house, Afternoon, Classic, Classic —ar 16:9`
-  }
-  else if (theme === "Professional") {
-    //  Mid-century Room
-      replicateMainPrompt = `Editorial Style [${room.toLowerCase()}] photo, Bird's eye, Mid-century modern, [${room.toLowerCase()}], Bar stools, Leather, wood, Wide shot of dining area, Earth tones, Design Within Reach, Natural light, Mid-century home, Morning, Retro, Mid-century modern —ar 16:9`
-  }
-  else if (theme === "Bohemian") {
-    //  Bohemian Room
-      replicateMainPrompt = `Editorial Style [${room.toLowerCase()}] photo, High angle, Bohemian, [${room.toLowerCase()}], Hanging plants, Textile, wood, Wide shot of [${room.toLowerCase()}], Earth tones, Anthropologie, Natural light, Beach house, Morning, Relaxing, Coastal —ar 16:9`
-  }
-  else if (theme === "Art Deco") {
-    //  Industrial Room
-      replicateMainPrompt = `Editorial Style [${room.toLowerCase()}] photo, Eye level, luxury, Art Deco, [${room.toLowerCase()}], Chandelier, Brass, marble, Close-up of glassware, Jewel tones, West Elm, Warm light, Art Deco building, Evening, Glamorous, Art Deco —ar 16:9`
-  }   
-  else if (theme === "Tropical") {
-  // Tropical Room
-    replicateMainPrompt = `Editorial Style [${room.toLowerCase()}] photo, Low angle, Tropical, [${room.toLowerCase()}], Antique stove, Ceramic, wood, Close-up of pie, Earthy tones, beach paradise, Pastel colors, Magnolia Home, Warm light, Farmhouse, Afternoon, Homey, Traditional —ar 16:9`
-  }
-  else {
-    // Generic 
-    replicateMainPrompt = `Editorial Style [${room.toLowerCase()}] photo, luxury, High angle, Modern, [${room.toLowerCase()}], Pendant lights, Glass, metal, Close-up of countertop, Monochromatic, Arhaus furniture collection, Cool light, Penthouse, Evening, Sleek, Contemporary —ar 16:9`
-  }
-  
   // POST request to Replicate to start the image restoration generation process
-  let startResponse = await fetch("httpapi.replicate.s://com/v1/predictions", {
+  let startResponse = await fetch("https://api.replicate.com/v1/predictions", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -134,9 +75,11 @@ export default async function handler(
       input: {
         image: imageUrl,
         prompt:
-          room === `${replicatePrompt}` || `${genericprompt}`,
+          room === "Gaming Room"
+            ? "a room for gaming with gaming computers, gaming consoles, and gaming chairs"
+            : `a ${theme.toLowerCase()} ${room.toLowerCase()}`,
         a_prompt:
-          '${replicateMainPrompt}',
+          `Editorial Style [${room.toLowerCase()}] photo, Williams-Sonoma, Natural light, Home, Morning, Cozy, Relaxing, Homey, [${theme.toLowerCase()}]`,
         n_prompt:
           "longbody, lowres, bad anatomy, bad hands, missing fingers, extra digit, fewer digits, cropped, worst quality, low quality",
       },
